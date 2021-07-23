166424d99f73a2396d480c51befe1e0754dd1755
Stan le Punk <stanislasiii@gmail.com>
2021-07-23 17:16:00 +0200

    cette fois je n'oublis pas de pousser mes affaires pour ne pas les perdre

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,31 +1,88 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { createContext, useContext } from "react";
 // Processing
 import P5Manager from "./P5Manager";
 import P5Wrapper from "./P5Wrapper";
 import { MenuButton } from "./menu_button";
 
+// https://www.robinwieruch.de/react-usecontext-hook
 const menu_style = size => {
   return {
     display: "grid",
     gridTemplateColumns: `repeat(auto-fill, minmax(${size}px, 1fr))`,
   };
 };
+// context
+export const MenuContext = createContext(null);
+export const ButtonContext = createContext(null);
 
 // transform, parse object list to REACT COMPONENT
-function MenuElem({ list }) {
+function MenuElem({ list, active_index, set_active_index }) {
+  console.log("3 MenuElem", active_index);
+  // results
   const res = [];
-  list.map(elem =>
+  let available = true;
+  list.map((elem, index) => {
+    // console.log("index", index, "active_index", active_index);
     res.push(
-      <MenuButton comp={elem.comp} label={elem.label} what={elem.what} />
-    )
-  );
+      <MenuContext.Provider
+        key={index}
+        value={{ index, active_index, set_active_index }}
+      >
+        <ButtonContext.Provider value={{ available }}>
+          <MenuButton
+            index={index}
+            comp={elem.comp}
+            label={elem.label}
+            what={elem.what}
+            menu={elem.menu}
+          />
+        </ButtonContext.Provider>
+      </MenuContext.Provider>
+    );
+  });
+  return <>{res}</>;
+}
+
+function MenuSub({ elem, active_index }) {
+  const { index } = useContext(MenuContext);
+  console.log("4 MenuSub", active_index);
+  // console.log("index, active_index", index, active_index);
+  if (index === active_index) {
+    return <div>{elem.label}</div>;
+  } else {
+    return <div></div>;
+  }
+}
+
+function MenuDeploy({ list, active_index }) {
+  const res = [];
+  list.map((elem, index) => {
+    res.push(
+      <MenuContext.Provider key={index} value={{ index, active_index }}>
+        <MenuSub elem={elem} active_index={active_index} />
+      </MenuContext.Provider>
+    );
+  });
   return <>{res}</>;
 }
 
 export function Menu({ content }) {
-  const names = ["0", "1", "2"];
+  // console.log(content);
+  console.log("0 Menu", new Date().getTime());
   const [button, set_button] = useState([]);
+  const [active_index, set_active_index] = useState(-1);
+  const [menu_index, set_menu_index] = useState(-1);
+  console.log("1 Menu", active_index, new Date().getTime());
+
+  // keep result in memory
+  useEffect(() => {
+    if (active_index >= 0) {
+      set_menu_index(active_index);
+      console.log("2 Menu useEffect", active_index);
+    }
+  }, [active_index]);
 
   // SET MENU
   if (button.length === 0) {
@@ -34,6 +91,7 @@ export function Menu({ content }) {
         comp: P5Wrapper(elem.name),
         label: elem.label,
         what: elem.what,
+        menu: elem.menu,
       };
       button.push(obj);
       set_button(button);
@@ -45,8 +103,13 @@ export function Menu({ content }) {
     <div>
       <P5Manager>
         <div style={menu_style(100)}>
-          <MenuElem list={button} />
+          <MenuElem
+            list={button}
+            active_index={active_index}
+            set_active_index={set_active_index}
+          />
         </div>
+        <MenuDeploy list={button} active_index={active_index} />
       </P5Manager>
     </div>
   );
--- src/components/menu_button.js
+++ src/components/menu_button.js
@@ -4,39 +4,60 @@ import { useState, useContext } from "react";
 // GATSBY
 import { navigate } from "gatsby";
 // PROCESSING
+import { button } from "../processing/button";
 import { P5DispatchContext, P5StateContext } from "./P5Manager";
+import { Menu } from "./menu";
+// MENU
+import { MenuContext } from "./menu";
+import { ButtonContext } from "./menu";
 
 export function MenuButton(props) {
-  // context data
+  // context button
+  const { index, active_index, set_active_index } = useContext(MenuContext);
+  let { available } = useContext(ButtonContext);
+  // let is_active = index === active_index;
+  // let { available } = useContext(ButtonContext);
+  // context procesing
   const dispatch = useContext(P5DispatchContext);
-  const { x, y, z } = useContext(P5StateContext);
+  // const { x, y, z } = useContext(P5StateContext);
   const [state_x, set_state_x] = useState(0);
   //sketch data
   let buf_data = {
     title: props.label,
   };
+  // console.log("buf_data", buf_data);
 
   const what_can_i_do = event => {
+    // context menu
+    // const { index, active_index, set_active_index } = useContext(MenuContext);
+    // let is_active = index === active_index;
+    // let { available } = useContext(ButtonContext);
+
     event.preventDefault();
 
     if (typeof props.what === "string" || props.what instanceof String) {
+      // selected
+      if (available) {
+        // console.log("je suis là -------------------------------");
+        // console.log("je suis là active index", active_index);
+        set_active_index(index);
+      }
+      // rest
       if (props.what.startsWith("/")) {
         if (props.what === "/back") {
           navigate(-1);
         } else {
           navigate(props.what);
         }
-      } else if (props.what.startsWith("add")) {
-        if (props.what === "add_x") {
-          const buf = state_x + 1;
-          set_state_x(buf);
-          dispatch({ type: "SET_X", payload: buf });
-        }
-      } else if (props.what.startsWith("sub")) {
-        if (props.what === "sub_x") {
-          const buf = state_x - 1;
-          set_state_x(buf);
-          dispatch({ type: "SET_X", payload: buf });
+      } else if (props.what.startsWith("submenu")) {
+        if (props.what === "submenu") {
+          // console.log("props.what", props.what);
+          // console.log("props.menu", props.menu);
+          // return (
+          //   <header>
+          //     <Menu content={props.menu} />
+          //   </header>
+          // );
         }
       }
     }
@@ -44,84 +65,7 @@ export function MenuButton(props) {
 
   return (
     <div onClick={what_can_i_do}>
-      <props.comp sketch={sketch_button} data={buf_data}></props.comp>
+      <props.comp sketch={button} data={buf_data}></props.comp>
     </div>
   );
 }
-
-/**
- *
- * Your sketch to design your button
- *
- */
-function sketch_button(p5) {
-  let pos = p5.createVector(0, 0);
-  let size = p5.createVector(0, 0);
-  let rounded = 0;
-  let inside_is = false;
-
-  p5.setup = function () {
-    p5.createCanvas(150, 100);
-
-    size.set(p5.width / 2, p5.height / 2);
-    pos.set(size.x / 2, size.y / 2);
-    rounded = p5.height / 2;
-  };
-
-  p5.draw = function () {
-    let cursor = p5.createVector(p5.mouseX, p5.mouseY);
-
-    inside_is = inside_rect(cursor, pos, size);
-    p5.clear();
-    if (inside_is) {
-      animation_in(pos, size, rounded);
-    } else {
-      animation_out(pos, size, rounded);
-    }
-    show_label();
-  };
-
-  function animation_in(s) {
-    let ratio = p5.abs(p5.sin(p5.frameCount * 0.05));
-
-    let diam = p5.map(ratio, 0, 1, p5.height / 5, p5.height / 2);
-    let x = p5.width / 2;
-    let y = p5.height / 2;
-    p5.noStroke();
-    p5.fill("cyan");
-    p5.ellipse(x, y, diam, diam);
-  }
-
-  function animation_out(p, s, r) {
-    p5.noStroke();
-    p5.fill("magenta");
-    p5.rect(p.x, p.y, s.x, s.y, r);
-  }
-
-  function show_label() {
-    p5.noStroke();
-    p5.fill(0);
-    p5.textAlign(p5.CENTER, p5.CENTER);
-    p5.text(p5.data.title, p5.width / 2, p5.height / 2);
-  }
-}
-
-export const inside_rect = function (cursor, pos, size) {
-  let x = cursor.x;
-  let y = cursor.y;
-  let px = pos.x;
-  let py = pos.y;
-  let sx = size.x;
-  let sy = size.y;
-  let bool_x = false;
-  if (x < px + sx && x > px) {
-    bool_x = true;
-  }
-  let bool_y = false;
-  if (y < py + sy && y > py) {
-    bool_y = true;
-  }
-
-  if (bool_x && bool_y) return true;
-  return false;
-};
