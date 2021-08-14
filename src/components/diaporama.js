b81f58b0989e36364dce0a145f11381eb00237c3
Stan le Punk <stanislasiii@gmail.com>
2021-08-14 18:35:39 +0200

    diaporama 2.0

--- src/components/diaporama.js
+++ src/components/diaporama.js
@@ -1,10 +1,12 @@
 import * as React from "react";
 
-import { useState } from "react";
+import { useState, createContext, useContext } from "react";
 
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 import "../css/diaporama.css";
+
+export const DiapoContext = createContext(null);
 
 function Title({ title, is, index, current, node }) {
   if (is === true) {
@@ -17,40 +19,63 @@ function Title({ title, is, index, current, node }) {
 }
 
 function Button({ click, name }) {
-  return <button onClick={click}>{name}</button>;
+  return (
+    <button className="button_nav" onClick={click}>
+      {name}
+    </button>
+  );
 }
 
-export default Button;
+function Console({ allFile, setting }) {
+  const { current, set_current } = useContext(DiapoContext);
 
-// scrimba diaporama
-// https://scrimba.com/scrim/cpqd9rta
-export function Diaporama({ allFile, background }) {
-  const style = {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    textAlign: "center",
-    background: background,
-  };
-
-  const [current, set_current] = useState(0);
-
-  const goto_next = () => {
-    if (current < allFile.edges.length) {
+  const go_next = () => {
+    if (current < allFile.edges.length - 1) {
       set_current(current + 1);
     } else {
       set_current(0);
     }
   };
 
-  const goto_previous = () => {
-    if (current >= 0) {
+  const go_first = () => {
+    set_current(0);
+  };
+
+  const go_last = () => {
+    set_current(allFile.edges.length - 1);
+  };
+
+  const go_previous = () => {
+    if (current > 0) {
       set_current(current - 1);
     } else {
       set_current(allFile.edges.length - 1);
     }
   };
+
+  return (
+    <div>
+      <Button click={go_first} name={setting.first} />
+      <Button click={go_previous} name={setting.previous} />
+      <Button click={go_next} name={setting.next} />
+      <Button click={go_last} name={setting.last} />
+    </div>
+  );
+}
+
+// scrimba diaporama
+// https://scrimba.com/scrim/cpqd9rta
+export function Diaporama({ allFile, setting }) {
+  const style = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+    background: setting.background,
+  };
+
+  const [current, set_current] = useState(0);
 
   // security
   if (!Array.isArray(allFile.edges) || allFile.edges.length <= 0) {
@@ -58,66 +83,30 @@ export function Diaporama({ allFile, background }) {
   }
 
   return (
-    <div style={style}>
-      <Button click={goto_next} name="next" />
-      <Button click={goto_previous} name="previous" />
-      {allFile.edges.map(({ node }, index) => (
-        <div>
-          <Title
-            title={node.base.split("_").join(" ").split(".")[0]}
-            is={index === current}
-            node={node}
-            index={index}
-            current={current}
-          />
-          <div
-            className={index === current ? "slide active" : "slide"}
-            key={node.id}
-            aria-hidden={index !== current}
-          >
-            {index === current && (
-              <GatsbyImage image={getImage(node)} alt={node.base} />
-            )}
+    <DiapoContext.Provider value={{ current, set_current }}>
+      <div style={style}>
+        {allFile.edges.map(({ node }, index) => (
+          <div>
+            <Title
+              title={node.base.split("_").join(" ").split(".")[0]}
+              is={index === current}
+              node={node}
+              index={index}
+              current={current}
+            />
+            <div
+              className={index === current ? "slide active" : "slide"}
+              key={node.id}
+              aria-hidden={index !== current}
+            >
+              {index === current && (
+                <GatsbyImage image={getImage(node)} alt={node.base} />
+              )}
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+        <Console allFile={allFile} setting={setting} />
+      </div>
+    </DiapoContext.Provider>
   );
 }
-
-// import * as React from "react";
-// https://www.youtube.com/watch?v=GOg6u6h_aqQ
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import Carousel from "react-bootstrap/Carousel";
-// import { GatsbyImage, getImage } from "gatsby-plugin-image";
-
-// export function Diaporama({ allFile, background }) {
-//   const style = {
-//     display: "flex",
-//     flexDirection: "column",
-//     justifyContent: "center",
-//     alignItems: "center",
-//     textAlign: "center",
-//     background: background,
-//     // minHeight: "100vh",
-//   };
-//   return (
-//     <div style={style}>
-//       <Carousel fade nextLabel="" prevLabel="">
-//         {allFile.edges.map(({ node }) => (
-//           <Carousel.Item key={node.id}>
-//             <GatsbyImage
-//               // className="d-block w-100"
-//               image={getImage(node)}
-//               alt={node.base}
-//             />
-//             <Carousel.Caption>
-//               <h3>{node.base.split("_").join(" ").split(".")[0]}</h3>
-//               {/* <p>et autres bêtises</p> */}
-//             </Carousel.Caption>
-//           </Carousel.Item>
-//         ))}
-//       </Carousel>
-//     </div>
-//   );
-// }
--- src/pages/photo.js
+++ src/pages/photo.js
@@ -7,24 +7,6 @@ import { Diaporama } from "../components/diaporama";
 
 import { useStaticQuery, graphql } from "gatsby";
 
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import Carousel from "react-bootstrap/Carousel";
-// import { GatsbyImage, getImage } from "gatsby-plugin-image";
-
-// function Diaporama({ allFile, background }) {
-//   return (
-//     <div style={{ background: background }}>
-//       <Carousel fade>
-//         {allFile.edges.map(({ node }) => (
-//           <Carousel.Item key={node.id}>
-//             <GatsbyImage image={getImage(node)} alt={node.base} />
-//           </Carousel.Item>
-//         ))}
-//       </Carousel>
-//     </div>
-//   );
-// }
-
 const Photo = () => {
   const { allFile } = useStaticQuery(
     graphql`
@@ -56,13 +38,21 @@ const Photo = () => {
     `
   );
 
+  const setting = {
+    background: "black",
+    first_is: true,
+    first: "prems",
+    last_is: false,
+    last: "der",
+    previous_is: true,
+    previous: "précédente",
+    next_is: true,
+    next: "suivante",
+  };
+
   return (
     <Layout>
-      <Diaporama allFile={allFile} background="black" />
-
-      {/* <div>
-        <GridPhoto />
-      </div> */}
+      <Diaporama allFile={allFile} setting={setting} />
     </Layout>
   );
 };
